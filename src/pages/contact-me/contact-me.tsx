4efc6b9627cc258943ca178d02d326efd262b5db
matthew weiler <mattweilerbusiness@gmail.com>
2020-02-02 15:28:45 -0600

    adding contact-me content

--- src/pages/contact-me/contact-me.tsx
+++ src/pages/contact-me/contact-me.tsx
@@ -1,24 +1,41 @@
 import React, { useState } from "react";
+import Grid from '@material-ui/core/Grid';
 import "./contact-me.css";
 import Form from '../../components/Form/Form';
 import Button from '../../components/Button/Button';
+import Title from '../../components/Title/Title';
 
 const ContactMe = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [description, setDescription] = useState('');
 
-  function handleSubmit() {
-  }
+    function handleSubmit() {
+        if (checkFields()) {
 
-  return (
-    <div className="contact-me">
-      <Form label="Name" placeholder="Name" onChange={setName} />
-      <Form label="Email" placeholder="Email Address" onChange={setName} />
-      <Form label="Description" placeholder="Description" onChange={setName} />
-        <Button text="Submit" onClick={handleSubmit} />
-    </div>
-  );
+        }
+        // make api call
+    }
+
+    function checkFields() {
+        return !!name && !!email && !!description;
+    }
+
+    return (
+        <Grid container item className="contact-me" direction="column" justify="space-evenly" >
+            <Grid item>
+                <Title text="Contact Me!" />
+            </Grid>
+            <Grid item>
+                <Form label="Name" placeholder="Name" onChange={setName} />
+                <Form label="Email" placeholder="Email Address" onChange={setEmail} />
+                <Form label="Description" placeholder="Description" onChange={setDescription} />
+            </Grid>
+            <Grid item xs={2}>
+                <Button text="Submit" onClick={handleSubmit} disabled={checkFields()} />
+            </Grid>
+        </Grid>
+    );
 };
 
 export default ContactMe;
